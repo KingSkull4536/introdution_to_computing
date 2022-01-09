26c2153e7a14c1bddcaa4981b7c9bcee522f5da0
KingSkull4536 <96479035+KingSkull4536@users.noreply.github.com>
2022-01-09 16:13:31 +0530

    Add files via upload

---
+++ Assignment1_computing_results.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================ RESTART: C:\Users\prash\AppData\Local\Programs\Python\Python310\Assignment1_computing.py ===============================
+Enter the first number: 5
+Enter the second number: 10
+Enter the third number: 12
+The average of given numbers is  9.0
+Enter your total income:50000
+Enter your none earning family members:15
+You don't have to pay income tax.
+Enter your SID-21105072
+Enter your name-Prashant jyoti
+Enter your Gender-(M,F,U) -  M
+Enter the course name-ECE
+Enter the CGPA-0
+[21105072, 'Prashant jyoti', 'M', 'ECE', 0.0]
+Enter your marks:15
+Enter your marks:12
+Enter your marks:13
+Enter your marks:18
+Enter your marks:90
+[12.0, 13.0, 15.0, 18.0, 90.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple']
