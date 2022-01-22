88fd971732a280bbc8af88968aa092652f1042db
Prashant jyoti <96479035+KingSkull4536@users.noreply.github.com>
2022-01-22 23:45:49 +0530

    Add files via upload

---
+++ Assignment2_computing_results.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\prash\AppData\Local\Programs\Python\Python310\Assignment2_computing.py
+Length of the given string is  35
+When written backwards the string becomes  egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Enter your name-Prashant Jyoti
+Enter your student ID-21105072
+Enter your opted branch/program-ECE
+Enter the CGPA:8.6
+Hey, Prashant Jyoti Here!
+      My SID is 21105072
+      I am from ECE department and my CGPA is 8.600000
+a&b =  8
+a|b =  58
+a^b =  50
+On shifting the binary representation of 56 leftwards twice, it becomes 224
+On shifting the binary representation of 10 leftwards twice, it becomes 40
+14
+0
+Enter the first number:4
+Enter the second number:6
+Enter the third number:8
+The largest number entered by you is  8
+Type the input:manemeannama
+No
+Enter the length of first side of triangle:3
+Enter the length of second side of triangle:4
+Enter the length of third side of triangle:5
+Yes
