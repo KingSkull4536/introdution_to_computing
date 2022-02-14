a37478d0e2068f82f15294e7839ff28b6c72e72b
Prashant jyoti <96479035+KingSkull4536@users.noreply.github.com>
2022-02-14 22:36:50 +0530

    Add files via upload

---
+++ Assignment3_computing_results.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/prash/AppData/Local/Programs/Python/Python310/Assignment3_computing.py
+Enter the string: Hello HELLO hell
+The occurence(s) of:
+'hello' is/are 2
+'hell' is/are 1
+Kindly enter natural numbers only. If fractional number is added then it will be rounded of to lesser integer
+Day - 29
+Month - 02
+Year - 2021
+The given month has only 28 days
+Please enter a valid date
+Day - 28
+Month - 02
+Year - 2000
+Next Date is: 29/2/2000
+Enter the list of integers separated by commas:[1,5]
+The list with their squares- [(1, 1), (5, 25)]
+Enter the grade of student: 7
+Your Grade is 'B' and Good Performance
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Enter the name of the student: Prashant jyoti
+Enter the SID of Prashant jyoti: 21105072
+
+You have entered 1 value(s) till now
+Do you want to enter more data? Enter Y if yes otherwise enter N:n
+
+a. Student Details:
+The SID of Prashant jyoti is 21105072
+
+b. Student Details (sorted with respect to names):
+The SID of Prashant jyoti is 21105072
+
+c. Student Details (sorted with respect to SIDs):
+The SID of Prashant jyoti is 21105072
+
+d. Enter the SID of the student: 21105073
+The SID you entered isn't entered
+Please enter a valid SID to be searched
+List of valid SIDs: [21105072]
+
+Enter the SID of the student: 21105072
+The name of student whose SID is 21105072 is Prashant jyoti
+Enter the no. of terms of the Fibonacci sequence: 8
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 
+The average of the terms of Fibonacci sequence upto 8 terms is 4.12
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
